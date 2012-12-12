b940825e222c014fe841b9ff6498e2cc3e237fbe
Curtis Badke <curtis.badke@devfacto.com>
2012-12-12 12:39:27 -0700

    Add GOL rules

---
+++ LifeEngine.Tests/Rules.Tests.fs
@@ -0,0 +1,39 @@
+﻿module Rules
+
+open NUnit.Framework
+open FsUnit
+open LifeEngine
+
+[<TestFixture>]
+type ``RulesTests``() =
+
+    [<Test>]
+    member tests.
+     ``Living Cells Die with 0 or 1 neighbours``() =
+        Rules.NextState Rules.Alive 0 |> should equal Rules.Dead
+        Rules.NextState Rules.Alive 1 |> should equal Rules.Dead
+
+    [<Test>]
+    member tests.
+     ``Living Cells Live with 2 or 3 neighbours`` () =
+        Rules.NextState Rules.Alive 2 |> should equal Rules.Alive 
+        Rules.NextState Rules.Alive 3 |> should equal Rules.Alive
+
+    [<Test>]
+    member tests.
+     ``Living Cells Die with >4 neighbours`` () =
+        Rules.NextState Rules.Alive 4 |> should equal Rules.Dead
+        Rules.NextState Rules.Alive 5 |> should equal Rules.Dead
+        Rules.NextState Rules.Alive 10 |> should equal Rules.Dead
+
+    [<Test>]
+    member tests.
+     ``Dead Cells come to life with 3 neighbours`` () =
+        Rules.NextState Rules.Dead 3 |> should equal Rules.Alive
+
+    [<Test>]
+    member tests.
+     ``Dead Cells stay dead for all other neighbours`` () =
+        Rules.NextState Rules.Dead 1 |> should equal Rules.Dead
+        Rules.NextState Rules.Dead 2 |> should equal Rules.Dead
+        Rules.NextState Rules.Dead 4 |> should equal Rules.Dead
---
+++ LifeEngine/Rules.fs
@@ -0,0 +1,11 @@
+﻿namespace LifeEngine
+
+module Rules =
+
+    type State = Dead|Alive
+
+    let NextState currentState count = 
+        match count with
+        | 2 -> currentState
+        | 3 -> Alive
+        | _ -> Dead
