2cad072f3ef3b6df44ff0a7161e1f0e3b5929b0a
Curtis Badke <curtis.badke@devfacto.com>
2012-12-12 08:10:27 -0700

    Coord class can identify neighbours

--- LifeEngine.Tests/Coord.Tests.fs
+++ LifeEngine.Tests/Coord.Tests.fs
@@ -5,9 +5,42 @@ open FsUnit
 open LifeEngine
 
 [<TestFixture>]
-type ``Coord`` ()=
-    let c = LifeEngine.Coord()  
+type ``Coordinate Object`` ()=
 
-    [<Test>] member test.
+    [<Test>]
+    member test.
+     ``Can be instantiated with ints``() =
+        let c = Coord(5L, 9L)
+        c.X |> should equal 5L
+        c.Y |> should equal 9L
+
+    [<Test>]
+    member test.
      ``Identifies itself as non-neighbour``() =
+        let c = Coord()
         c.IsNeighbour c |> should be False
+
+    [<Test>]
+    member test.
+     ``Identifies horizontal neighbour as neighbour``() =
+        let c = Coord(0L, 0L)
+        let left = Coord(-1L, 0L)
+        let right = Coord(1L, 0L)
+        c.IsNeighbour left |> should be True
+        c.IsNeighbour right |> should be True
+
+    [<Test>]
+    member test.
+     ``Identifies vertical neighbour as neighbour``() =
+        let c = Coord()
+        let up = Coord(0L, 1L)
+        let down = Coord(0L, -1L)
+        c.IsNeighbour up |> should be True
+        c.IsNeighbour down |> should be True
+
+    [<Test>]
+    member test.
+     ``Identifies 'row-neighbour' as non-neighbour`` () =
+        let c = Coord()
+        let upRow = Coord(10L, 1L)
+        c.IsNeighbour upRow |> should be False
--- LifeEngine/Coord.fs
+++ LifeEngine/Coord.fs
@@ -1,4 +1,16 @@
 ﻿namespace LifeEngine
 
-type Coord() =
-    member x.IsNeighbour(coord) = false
+open System
+
+type Coord(x, y) =
+    member this.IsNeighbour(coord : Coord) =
+        match coord with
+        | _ when x = coord.X && y = coord.Y -> false
+        | _ when Math.Abs(x - coord.X : int64) <= 1L &&
+                 Math.Abs(y - coord.Y : int64) <= 1L -> true
+        | _ -> false
+
+    member this.X = x
+    member this.Y = y
+
+    new() = Coord(0L, 0L)
