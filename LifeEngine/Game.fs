855dea12abac05d83073acaff110f97dac688f8a
Curtis Badke <curtis.badke@devfacto.com>
2012-12-13 14:37:14 -0700

    Reorg grid constructors.
    Gives new seq<Coord> constructor which can be used to generate new generations of grids and hides the string parsing code into the specific seq<string> constructor.

--- LifeEngine/Game.fs
+++ LifeEngine/Game.fs
@@ -1,43 +1,38 @@
 ﻿namespace LifeEngine
 
-type Game(collection : seq<string>) =
-    let parseString (idx : int) str = str 
-                                      |> Seq.fold (fun (column, rowCol) ch -> 
-                                            if ch <> '0' then 
-                                                (column+1L, Coord(column,int64 idx)::rowCol) 
-                                            else 
-                                                (column+1L, rowCol)) 
-                                            (0L, List.Empty)
-    let grid = collection
-               |> Seq.mapi parseString
-               |> Seq.map (fun (a,b) -> b)
-               |> Seq.fold (fun gr row -> if List.length row > 0 then row::gr else gr) List.Empty
+type Game(cells : seq<Coord>) =
 
     let gridTop g = g
-                    |> Seq.map (fun row -> row
-                                           |> Seq.map (fun (c : Coord) -> c.Y)
-                                           |> Seq.min)
+                    |> Seq.map (fun (c : Coord) -> c.Y)
                     |> Seq.min
     let gridBottom g = g
-                       |> Seq.map (fun row -> row
-                                              |> Seq.map (fun (c : Coord) -> c.Y)
-                                              |> Seq.max)
+                       |> Seq.map (fun (c : Coord) -> c.Y)
                        |> Seq.max
     let gridLeft g = g
-                     |>   Seq.map (fun row -> row
-                                              |> Seq.map (fun (c : Coord) -> c.X)
-                                              |> Seq.min)
+                     |> Seq.map (fun (c : Coord) -> c.X)
                      |> Seq.min
     let gridRight g = g
-                      |> Seq.map (fun row -> row
-                                             |> Seq.map (fun (c : Coord) -> c.X)
-                                             |> Seq.max)
+                      |> Seq.map (fun (c : Coord) -> c.X)
                       |> Seq.max
     
-    member x.Extents = match grid with
-                       | _ when List.isEmpty grid -> (0L,0L)
-                       | _ -> (gridRight grid - gridLeft grid + 1L, gridBottom grid - gridTop grid + 1L)
+    member x.Extents = match cells with
+                       | _ when Seq.isEmpty cells -> (0L,0L)
+                       | _ -> (gridRight cells - gridLeft cells + 1L, gridBottom cells - gridTop cells + 1L)
 
+    member x.LiveCells = cells
 
-    member x.LiveCells = Seq.concat grid
-    new() = Game(Seq.empty)
+    new(collection : seq<string>) =
+        let parseString (idx : int) str = str 
+                                          |> Seq.fold (fun (column, rowCol) ch -> 
+                                                if ch <> '0' then 
+                                                    (column+1L, Coord(column,int64 idx)::rowCol) 
+                                                else 
+                                                    (column+1L, rowCol)) 
+                                                (0L, List.Empty)
+        let coords = collection
+                    |> Seq.mapi parseString
+                    |> Seq.map (fun (a,b) -> b)
+                    |> Seq.fold (fun gr row -> if List.length row > 0 then row::gr else gr) List.Empty
+                    |> Seq.concat
+        Game(coords)
+    new() = Game(Seq.empty<Coord>)
